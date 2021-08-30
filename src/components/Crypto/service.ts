04d3b068295a1d50c172ce587bdba882002df550
Komron Bakozoda <kbakozoda@twilio.com>
2021-08-30 04:06:36 +0200

    Implement passing failed message to retry manager in a separate method

--- src/components/Crypto/service.ts
+++ src/components/Crypto/service.ts
@@ -1,10 +1,12 @@
 import HttpError from "../../config/error";
 import CacheService, { ICacheGetKeyResult } from "../../services/Cache";
+import SignRetryManager, { IRetryQueueItem } from "../../services/SignRetryManager";
 import SynthesiaService, { ISynthesiaActionResult } from "../../services/Synthesia";
+import UserWebhookService from "../../services/UserWebhook";
 import { ICryptoService } from "./interface";
 
 const CryptoService: ICryptoService = {
-    async signMessage(message: string): Promise<any> {
+    async signMessage(message: string, userId: string): Promise<any> {
         console.log("New attempt to sign a message, checking the cache", message);
         const cacheGetKeyResult: ICacheGetKeyResult = await CacheService.getValue(message); 
         let signVerificationResult: ISynthesiaActionResult;
@@ -31,10 +33,30 @@ const CryptoService: ICryptoService = {
                 return result.data;
             } else {
                 console.log("Could not sign a message");
+                await CryptoService.passFailedMessageToRetryManager(message, userId);
                 throw new HttpError(418, "Retry later");
             }
         }
     },
+
+    async passFailedMessageToRetryManager(message: string, userId: string): Promise<any> {
+        let webhook;
+        try {
+            webhook = await UserWebhookService.getUserWebhook(userId);
+        } catch(err) {
+            console.log("could not get webhook for user", userId);
+            console.log(err);
+        }
+
+        const itemForQueue: IRetryQueueItem = {
+            userId,
+            message,
+            webhook
+        };
+
+        console.log("Pushing message to retry queue");
+        await SignRetryManager.addItemToRetryQueue(itemForQueue);
+    }
 };
 
 export default CryptoService;
